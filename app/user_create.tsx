14fc88030191e8f8d1e9f6abcf465b22b2e9771e
Edemilton Alcides Galindo Junior <edemilton.junior@EDU.gruposete.local>
2026-03-19 19:07:50 -0300

    Implementação incial da tela de login e criação de usuário do app

---
+++ app/user_create.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+
+
+export default function CreateUser() {
+    const [nome, setNome] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Criar Usuário</Text>
+
+            <TextInput
+                style={styles.input}
+                value={nome}
+                onChangeText={setNome}
+                placeholder='Nome'
+            />
+
+            <TextInput
+                style={styles.input}
+                value={email}
+                onChangeText={setEmail}
+                placeholder='E-mail'
+            />
+
+            <TextInput
+                style={styles.input}
+                value={password}
+                onChangeText={setPassword}
+                placeholder='Senha'
+                secureTextEntry={true}
+            />
+
+            <TouchableOpacity
+                style={styles.button}
+            >
+                <Text style={styles.textButton}>Criar usuário</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    titulo: {
+        color: "#fff",
+        fontSize: 32,
+        marginBottom: 50
+    },
+    container: {
+        backgroundColor: "#F60",
+        padding: 30,
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    alert: {
+        fontSize: 18,
+        textAlign: 'center',
+        color: '#FFF',
+        marginBottom: 20,
+    },
+    input: {
+        fontSize: 18,
+        borderRadius: 10,
+        backgroundColor: '#FFF',
+        padding: 20,
+        marginBottom: 20,
+        width: '100%'
+    },
+    button: {
+        backgroundColor: '#070A52',
+        padding: 10,
+        borderRadius: 10,
+        marginBottom: 20,
+        width: '100%'
+    },
+    textButton: {
+        fontSize: 24,
+        textAlign: 'center',
+        color: '#fff'
+    }
+});
